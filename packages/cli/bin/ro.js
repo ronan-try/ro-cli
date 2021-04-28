f808f48a52dcba9dd4287e4a01bf72a67568ff02
Ronan <liruonan@hotmail.com>
2021-04-28 17:56:17 +0800

    feat: 破釜沉舟 搬家ro-cli

--- packages/cli/bin/ro.js
+++ packages/cli/bin/ro.js
@@ -14,54 +14,54 @@ program
 program
   .command('add')
   .description(`add project's local path`)
-  .action(() => require('../lib/add')());
+  .action(() => require('../lib/pro-add')());
 
 program
   .command('list')
   .description('table the cached projects')
-  .action(() => require('../lib/list')());
+  .action(() => require('../lib/pro-list')());
 
 program
   .command('delete')
   .description(`delete project's local path`)
-  .action(() => require('../lib/del')());
+  .action(() => require('../lib/pro-del')());
 
 program
   .command('update')
   .description(`update such project's config`)
-  .action(() => require('../lib/update')());
+  .action(() => require('../lib/pro-update')());
 
 program
   .command('fork')
   .description('new a local-repo branch from a target-repo branch')
-  .action(() => require('../lib/fork')());
+  .action(() => require('../lib/git-fork')());
 
 program
   .command('sync')
   .description('sync the target branch to local branch')
-  .action(() => require('../lib/sync')());
+  .action(() => require('../lib/git-sync')());
 
 program
   .command('pr')
   .description('make a pr to target-repo')
-  .action(() => require('../lib/pr')());
+  .action(() => require('../lib/git-pr')());
 
 program
   .command('mr')
   .description('make a pr to target-repo')
-  .action(() => require('../lib/pr')());
+  .action(() => require('../lib/git-pr')());
 
 program
   .command('start')
   .description('new terminal to start your project')
-  .action(() => require('../lib/start')());
+  .action(() => require('../lib/pro-start')());
 
 program
   .command('open')
   .description('open a project folder/vs code')
   .option('-fd, --folder', 'open with folder')
   .option('-vs, --vscode', 'open with vscode')
-  .action(() => require('../lib/open')());
+  .action(() => require('../lib/pro-open')());
 
 program
   .command('diff <file-0> <file-1>', 'Compare two files with each other.');
@@ -69,13 +69,13 @@ program
 program
   .command('ui')
   .description('ui operation')
-  .action(() => require('../lib/ui')());
+  .action(() => require('../lib/git-ui')());
 
 program
   .command('ng1')
-  .description('价格系统ng1，发布test/master分支')
+  .description('ng1:本地打包test/master，发布test/master')
   .option('-t, --test', 'merge feature to test, build, push test')
   .option('-m, --master', 'merge feature to master, build, push master')
-  .action(() => require('../lib/ng1')());
+  .action(() => require('../lib/pro-ng1')());
 
 program.parse(process.argv);
