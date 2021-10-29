4047461a18579607a5ee4c14900a6e64c998b259
Ronan <liruonan@hotmail.com>
2021-10-29 11:25:30 +0800

    test(cli-cache): 新增test case

--- packages/cli-cache/__tests__/index.ts
+++ packages/cli-cache/__tests__/index.ts
@@ -3,7 +3,13 @@ import { getRawCacheData } from '../index'
 test('getRawCacheData:projects.cache.json', () => {
   const source = getRawCacheData('projects')
 
-  console.log(source)
-
   expect(Array.isArray(source)).toBe(true)
+});
+
+test('读取branchMap缓存文件', () => {
+  const source = getRawCacheData('branchMap');
+
+  console.log(typeof source, Object.prototype.toString.call(source));
+
+  expect(Array.isArray(source)).toBe(true);
 });
