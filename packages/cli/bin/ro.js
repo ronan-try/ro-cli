d429bd6780698cc345499b8db5230ec65d890e8a
Ronan <liruonan@hotmail.com>
2021-10-29 11:16:51 +0800

    update(ro.js): 更新中文说明

--- packages/cli/bin/ro.js
+++ packages/cli/bin/ro.js
@@ -7,68 +7,72 @@
 
 const { program } = require('commander');
 
+function  padRight (descChiness, descEnglish, len = 20) {
+  return `${descChiness}`.padEnd(len, ' ') + '// ' + descEnglish;
+}
+
 program
   .version(`${require('../package.json').version}`)
   .usage('<command> [options]');
 
 program
   .command('add')
-  .description(`添加本地项目到Cli/ add project's local path`)
+  .description(padRight('新增-本地项目信息', 'add new local projects'))
   .action(() => require('../lib/db-pro-add')());
 
 program
   .command('list')
-  .description('查看Cli所有本地项目/ table the cached projects')
+  .description(padRight('查看-本地项目信息', 'table the local projects'))
   .action(() => require('../lib/db-pro-list')());
 
 program
   .command('delete')
-  .description(`删除Cli中的本地项目/ delete project's local path`)
+  .description(padRight('删除-本地项目信息', 'delete one local project'))
   .action(() => require('../lib/db-pro-del')());
 
 program
   .command('update')
-  .description(`更新Cli中的本地项目/ update such project's config`)
+  .description(padRight('更新-本地项目信息', 'update such a local project'))
   .action(() => require('../lib/db-pro-update')());
 
 program
   .command('fork')
-  .description('Fork目标仓库分支到本地项目/ new a local-repo branch from a target-repo branch')
+  .description(padRight('fork目标分支 -> 个人分支', 'new a local-repo branch from a target-repo branch'))
   .action(() => require('../lib/git-fork')());
 
 program
   .command('sync')
-  .description('同步目标仓库分支到本地分支/ sync the target branch to local branch')
+  .description(padRight('同步目标分支 -> 个人分支', 'sync the target branch to local branch'))
   .action(() => require('../lib/git-sync')());
 
 program
   .command('pr')
-  .description('发起Pull request/ make a pr to target-repo')
+  .description(padRight('发起 PR', 'make a PR to target-repo'))
   .action(() => require('../lib/git-pr')());
 
 program
   .command('mr')
-  .description('发起Merge request/ make a pr to target-repo')
+  .description(padRight('发起 MR', 'make a MR to target-repo'))
   .action(() => require('../lib/git-pr')());
 
 program
   .command('start')
-  .description('启动本地项目/ new terminal to start your project')
+  .description('启动本地项目 // new terminal to start your project')
   .action(() => require('../lib/cli-start')());
 
 program
   .command('open')
-  .description('打开本地项目(-fd or -vs)/ open a project folder/vs code')
+  .description('打开本地项目(-fd or -vs) // open a project folder/vs code')
   .option('-fd, --folder', '使用资源目录打开/ open with folder')
   .option('-vs, --vscode', '使用VSCode打开/ open with vscode')
   .action(() => require('../lib/cli-open')());
 
 program
-  .command('diff <file-0> <file-1>', 'Compare two files with each other.');
+  .command('diff <file-0> <file-1>', '利用vscode 对比文件');
 
 program
   .command('ui')
-  .description('ui可视化点点点/ ui operation')
+  .description('ui可视化点点点 // ui operation')
   .action(() => require('../lib/git-ui')());
 
 program
